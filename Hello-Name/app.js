b8a1eca1c1d98f5c739d64cffb1d6fb7d2b568f3
geradolm <gerardo.marin@developyn.com>
2022-01-17 22:39:20 +0000

    Update Hello-Name

--- Hello-Name/app.js
+++ Hello-Name/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 4000
-const name = "Gerardo"
+// const name = "Gerardo"
 
-app.get('/', (req, res ) =>{
-    res.send(`Hello ${name}!`)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+// app.get('/', (req, res ) =>{
+//     res.send(`Hello ${name}!`)
+// })
+
+app.get('/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}!`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
